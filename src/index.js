e709b4d44778163da380859b27b564989ca9af9c
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-03-01 20:10:55 +0100

    faker npm not working

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const getTime = () => {
-  return new Date().toLocaleTimeString();
-};
-
 //Creamos o compoñente de React:
 const App = () => {
   return (
     <div>
-      <h1>{getTime()}</h1>
-      <h2>Hora actual:</h2>
+      <div className="ui container comments">
+        <div className="comment">
+            <a href="/" className="avatar">
+                <img src="/" alt="avatar" />
+            </a>
+            <div className="content">
+                <a href="/" className="author">Eu</a>
+            </div>
+            <div className="metadata">
+                <span className="date">28/2/2022 18:55:23</span>
+            </div>
+            <div className="text">
+                Blah blah blah!
+            </div>
+        </div>
+    </div>
     </div>
   );
 };
